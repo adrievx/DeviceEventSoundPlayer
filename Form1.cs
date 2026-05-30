bbf56b53c4ac097d4df87e0600770faaec4dc9e5
Adrien Vernier <177334560+adrievx@users.noreply.github.com>
2026-05-30 12:50:39 +0100

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Management;
+using System.Media;
+using System.Windows.Forms;
+
+namespace DeviceEventSoundPlayer {
+    public partial class Form1 : Form {
+        private readonly SoundPlayer connectSound = new SoundPlayer(@"C:\Windows\Media\Windows Hardware Insert.wav");
+        private readonly SoundPlayer disconnectSound = new SoundPlayer(@"C:\Windows\Media\Windows Hardware Remove.wav");
+
+        private ManagementEventWatcher insertWatcher;
+        private ManagementEventWatcher removeWatcher;
+
+        public Form1() {
+            InitializeComponent();
+
+            ShowInTaskbar = false;
+            WindowState = FormWindowState.Minimized;
+            Visible = false;
+
+            StartWatchers();
+        }
+
+        private void StartWatchers() {
+            insertWatcher = new ManagementEventWatcher(new WqlEventQuery("SELECT * FROM Win32_DeviceChangeEvent WHERE EventType = 2"));
+
+            insertWatcher.EventArrived += (s, e) => {
+                connectSound.Play();
+            };
+
+            insertWatcher.Start();
+
+            removeWatcher = new ManagementEventWatcher(new WqlEventQuery("SELECT * FROM Win32_DeviceChangeEvent WHERE EventType = 3"));
+
+            removeWatcher.EventArrived += (s, e) => {
+                disconnectSound.Play();
+            };
+
+            removeWatcher.Start();
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e) {
+            insertWatcher?.Stop();
+            removeWatcher?.Stop();
+
+            base.OnFormClosing(e);
+        }
+    }
+}
