27c43f7f51e74ddfaab07ecb821ae0e601364ee1
adrievx <adrievx@hotmail.com>
2026-05-30 13:00:16 +0100

    [Form1.cs] General code cleanup

--- Form1.cs
+++ Form1.cs
@@ -14,6 +14,7 @@ namespace DeviceEventSoundPlayer {
         public Form1() {
             InitializeComponent();
 
+            // ui state
             ShowInTaskbar = false;
             WindowState = FormWindowState.Minimized;
             Visible = false;
@@ -23,7 +24,6 @@ namespace DeviceEventSoundPlayer {
 
         private void StartWatchers() {
             insertWatcher = new ManagementEventWatcher(new WqlEventQuery("SELECT * FROM Win32_DeviceChangeEvent WHERE EventType = 2"));
-
             insertWatcher.EventArrived += (s, e) => {
                 connectSound.Play();
             };
@@ -31,7 +31,6 @@ namespace DeviceEventSoundPlayer {
             insertWatcher.Start();
 
             removeWatcher = new ManagementEventWatcher(new WqlEventQuery("SELECT * FROM Win32_DeviceChangeEvent WHERE EventType = 3"));
-
             removeWatcher.EventArrived += (s, e) => {
                 disconnectSound.Play();
             };
